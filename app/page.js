9bb5d913321ae50e4e0b58aa886ab246569dbb5b
Alima <alimausmani21@navgurukul>
2023-11-04 15:04:06 +0530

    Folder and File

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Todo_form from "./todo_form";
+import "./globals.css";
+
+const Page =()=>{
+  return(
+    <Todo_form/>
+  );
+}
+export default Page;
---
+++ app/todo_form.js
@@ -0,0 +1,6 @@
+const Todo_form =()=>{
+    return(
+        <h1>Todo_form</h1>
+    );
+}
+export default Todo_form;
