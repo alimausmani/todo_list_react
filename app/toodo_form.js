c3faadd975d53aa66d4c612122668b5e97963830
sushma <sushma21@navgurukul.org>
2023-11-11 12:35:10 +0530

    trying to add functionality

--- app/toodo_form.js
+++ app/toodo_form.js
@@ -1,11 +1,18 @@
+"use client"
+import {useState} from "react";
 const Toodo_form =()=>{
+    const [name, setName] = useState('');
+    const [artists, setArtists] = useState([]);
    return (
    <div className="input_div">
         <form>
             <h1 className="heading">TODO LIST</h1>
             <p className="heading1">A Simple todo list </p>
           <label className="name"><h1>New Todo</h1>
-            <input className="name_input" placeholder="Your Task"type="text" />
+            <input className="name_input" placeholder="Your Task"type="text" 
+            value={name}
+            onChange={e => setName(e.target.value)}
+            />
           </label>
           <div>
           <h1 className="selecter">Priority Task</h1>
@@ -19,7 +26,19 @@ const Toodo_form =()=>{
           <label className="Time"><h1>Time Limit</h1>
             <input className="Time_text" placeholder="Your Time" type="text" />
           </label>
-          <button className="btn">ADD TODO</button>
+          <button className="btn"
+          onClick={() => {
+          artists.push({
+          id: nextId++,
+          name: name,
+        });
+        }}>ADD TODO
+          </button>
+          <ul>
+            {artists.map(artist => (
+            <li key={artist.id}>{artist.name}</li>
+            ))}
+          </ul>
         </form>
    </div>     
   )
